cbd281c6eb41b4b9c6081105c09676bf01eb0bfd
Noirkun <noirkun2306@gmail.com>
2022-06-25 18:56:44 +0900

    作業更新（未完成） ドラック

--- gamehackathon-20220618/Assets/User/Suzuki/Mouse.cs
+++ gamehackathon-20220618/Assets/User/Suzuki/Mouse.cs
@@ -1,22 +1,116 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Mouse : MonoBehaviour
-
+public class Mouse : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    //オブジェクトをクリックしてドラッグ状態にある間呼び出される関数（Unityのマウスイベント）
-    void OnMouseDrag()
+    [SerializeReference]
+    private bool Flag1B = false;
+    [SerializeReference]
+    private bool Flag2A = false;
+    [SerializeReference]
+    private bool Flag1A = false;
+    [SerializeReference]
+    private bool Flag2B = false;
+    //　点数（スコア）
+    public int Acount = 0;
+    public int Bcount = 0;
+
+    private Vector2 prevPos; //保存しておく初期position
+    private RectTransform rectTransform; // 移動したいオブジェクトのRectTransform
+    private RectTransform parentRectTransform; // 移動したいオブジェクトの親(Panel)のRectTransform
+
+    private void Awake()
     {
-        //マウスカーソル及びオブジェクトのスクリーン座標を取得
-        Vector3 objectScreenPoint =
-            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        rectTransform = GetComponent<RectTransform>();
+        parentRectTransform = rectTransform.parent as RectTransform;
+    }
 
-        //スクリーン座標をワールド座標に変換
-        Vector3 objectWorldPoint = Camera.main.ScreenToWorldPoint(objectScreenPoint);
 
-        //オブジェクトの座標を変更する
-        transform.position = objectWorldPoint;
-        //https://qiita.com/ToneUs/items/0f45e1a427102d9e8a27
-        //https://qiita.com/moriba/items/bd485d2a2938b28484a9
+    // ドラッグ開始時の処理
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // ドラッグ前の位置を記憶しておく
+        //RectTransformの場合はpositionではなくanchoredPositionを使う
+        prevPos = rectTransform.anchoredPosition;
+
+    }
+
+    // ドラッグ中の処理
+    public void OnDrag(PointerEventData eventData)
+    {
+        // eventData.positionから、親に従うlocalPositionへの変換を行う
+        // オブジェクトの位置をlocalPositionに変更する
+
+        Vector2 localPosition = GetLocalPosition(eventData.position);
+        rectTransform.anchoredPosition = localPosition;
+    }
+
+    // ドラッグ終了時の処理
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Flag2A = true;
+        Flag2B = true;
+        if (Flag1A == true)
+        {
+            if (Flag2A == true)
+            {
+                Acount++;
+                Flag1A = false;
+                Flag2A = false;
+                // Destroy(this.gameObject);
+            }
+        }
+        if (Flag1B == true)
+        {
+            if (Flag2B == true)
+            {
+                Bcount++;
+                Flag1B = false;
+                Flag2B = false;
+                // Destroy(this.gameObject);
+            }
+        }
+        // オブジェクトをドラッグ前の位置に戻す
+        rectTransform.anchoredPosition = prevPos;
+        // Destroy(this.gameObject);
+
+    }
+
+    // ScreenPositionからlocalPositionへの変換関数
+    private Vector2 GetLocalPosition(Vector2 screenPosition)
+    {
+        Vector2 result = Vector2.zero;
+
+        // screenPositionを親の座標系(parentRectTransform)に対応するよう変換する.
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPosition, Camera.main, out result);
+
+        return result;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "A")
+        {
+            Flag1A = true;
+        }
+        else
+        {
+            Flag1A = false;
+        }
+        if (collision.gameObject.tag == "B")
+        {
+            Flag1B = true;
+        }
+        else
+        {
+            Flag1B = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerExit2D: " + other.gameObject.name);
     }
 }
