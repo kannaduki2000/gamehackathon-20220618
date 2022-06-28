b2360a4e445aeae553e90cc767f70bfba404f850
kikuchitoya <21c1020024kt@edu.tech.ac.jp>
2022-06-28 21:19:54 +0900

    Scoreの計算処理の作成、スコアマネージャー、スコア管理スクリプトの作成

--- gamehackathon-20220618/Assets/User/Kikuchi/Scripts/FirstTypeBox.cs
+++ gamehackathon-20220618/Assets/User/Kikuchi/Scripts/FirstTypeBox.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstTypeScoreManager : MonoBehaviour
+public class FirstTypeBox : MonoBehaviour
 {
     public int ResultScore = 0;
     [SerializeField]
     private int PlusScore = 100;
-    int SortedNumber = 0;
-    bool GameFinish = false;
     int DisplayCount =0;
 
 
@@ -30,36 +28,15 @@ public class FirstTypeScoreManager : MonoBehaviour
         if (collision.tag == "FirstType")
         {
             ResultScore += PlusScore;
-            SortedNumber++;
-        }
-
-        else if (collision.tag == "SecondType")
-        {
-            ResultScore -= PlusScore;
-            SortedNumber++;
-        }
-        Debug.Log(SortedNumber);
-
-        if(SortedNumber ==10)
-        {
-            GameFinish = true;
         }
 
     }
 
     public int ScoreResult()
     {
-        if (GameFinish == true && DisplayCount == 0)
-        {
             ++DisplayCount;
             Debug.Log(ResultScore);
             return ResultScore;
-        }
-            
-        else
-        {
-            return 0;
-        }
     }
 
 }
---
+++ gamehackathon-20220618/Assets/User/Kikuchi/Scripts/ScoreController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [HideInInspector] public int Score = 0;
+
+
+    //仕分けでスコアを加算する。
+    //スコアを画面左上に表示
+    //仕分けたものはデストロイ成功したら加算して
+    //デストロイ処理をするオブジェクトを置く。
+    //
+}
